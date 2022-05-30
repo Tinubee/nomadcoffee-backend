e3d18075bfd358fe28ede30e6d8da6dd33f19f45
KIMHYOUNGMIN <rlagud2005@naver.com>
2022-05-30 09:17:50 +0900

    Challenges #4

---
+++ users/followUser/followUser.resolvers.js
@@ -0,0 +1,34 @@
+import client from "../../client";
+import { protectedResolvers } from "../users.utils";
+
+export default {
+  Mutation: {
+    followUser: protectedResolvers(
+      async (_, { username }, { loggedInUser }) => {
+        const ok = await client.user.findUnique({ where: { username } });
+        if (!ok) {
+          return {
+            ok: false,
+            error: "User not found",
+          };
+        }
+
+        await client.user.update({
+          where: {
+            id: loggedInUser.id,
+          },
+          data: {
+            following: {
+              connect: {
+                username,
+              },
+            },
+          },
+        });
+        return {
+          ok: true,
+        };
+      }
+    ),
+  },
+};
